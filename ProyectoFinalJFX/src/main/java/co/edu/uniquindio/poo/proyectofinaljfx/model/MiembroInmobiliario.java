ac76dc863561e649600a0288774940888ea43aa6
Royerg Orozco <royergs.orozcoramirez@uqvitual.edu.co>
2026-05-22 00:17:48 -0500

    Correción método constructor

--- ProyectoFinalJFX/src/main/java/co/edu/uniquindio/poo/proyectofinaljfx/model/MiembroInmobiliario.java
+++ ProyectoFinalJFX/src/main/java/co/edu/uniquindio/poo/proyectofinaljfx/model/MiembroInmobiliario.java
@@ -13,7 +13,10 @@ public abstract class MiembroInmobiliario implements IPuntuable, IBeneficiable{
     protected RangoUsuario rangoUsuario;
     protected InmoSmart ownedByInmoSmart;
     protected Usuario usuario;
-    public MiembroInmobiliario(String id, String nombre, String identificacion, String telefono, String correo,String userName, String contrasenia, InmoSmart ownedByInmoSmart) {
+
+    public MiembroInmobiliario(String id, String nombre, String identificacion,
+                               String telefono, String correo,String userName, String contrasenia,
+                               InmoSmart ownedByInmoSmart) {
         this.id = id;
         this.nombre = nombre;
         this.identificacion = identificacion;
